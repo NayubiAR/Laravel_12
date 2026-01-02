1e3f68a0bc9533591bab8c14056a630caf3a2712
NayubiAR <nayubiadiva@gmail.com>
2026-01-03 01:32:48 +0700

    Menambahkan Halaman Rencana Langganan

---
+++ app/Http/Controllers/SubscribeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Plan;
+use Illuminate\Http\Request;
+
+class SubscribeController extends Controller
+{
+    public function showPlans(Request $request)
+    {
+        $plans = Plan::all();
+        return view('subscribe.plans', compact('plans'));
+    }
+}
---
+++ resources/views/subscribe/plans.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.subscribe')
+
+@section('title', 'Plan Detail')
+@section('page-title', 'Detail Rencana Langganan')
+
+@section('content')
+    <div class="mt-4 row justify-content-center gy-4">
+        @foreach ($plans as $plan)
+            <div class="col-12 col-md-4">
+                <div class="card h-100">
+                    <div class="card-header">
+                        <h3 class="mb-0 text-white">{{ $plan->title }}</h3>
+                        <div class="fs-4 fw-bold">Rp{{ number_format($plan->price, 0, ',', '.') }} / {{ $plan->duration }}
+                            hari
+                        </div>
+                    </div>
+                    <div class="card-body d-flex flex-column">
+                        <div class="mb-4">
+                            <h6 class="text-white">Resolusi</h6>
+                            <p class="mb-0 text-green">{{ $plan->resolution }}</p>
+                        </div>
+                        <div class="mb-4">
+                            <h6 class="text-white">Perangkat yang Support</h6>
+                            <p class="mb-0 text-green">Mobile, Computer, TV</p>
+                        </div>
+                        <div class="mb-4">
+                            <h6 class="text-white">Perangkat yang bisa dipakai secara bersamaan</h6>
+                            <p class="mb-0 text-green">{{ $plan->max_devices }} Perangkat</p>
+                        </div>
+                        {{-- <div class="mt-auto">
+                            <a href="{{ route('subscribe.checkout', $plan) }}" class="btn btn-green w-100">Pilih Rencana Langganan</a>
+                        </div> --}}
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+@endsection
+
+@section('scripts')
+@endsection
