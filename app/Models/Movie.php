69274039d358d7d510287446c0a9b9b88a894f27
NayubiAR <nayubiadiva@gmail.com>
2026-01-03 11:38:21 +0700

    Menambahkan Migration dan Seeder untuk Movie

---
+++ app/Models/Movie.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'director',
+        'writers',
+        'stars',
+        'poster',
+        'release_date',
+        'duration',
+        'url_720',
+        'url_1080',
+        'url_4k',
+    ];
+
+    protected $appends = ['average_rating'];
+
+    protected $casts = [
+        'release_date' => 'date',
+    ];
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             CategorySeeder::class,
         ]);
+        $this->call([
+            MovieSeeder::class,
+        ]);
     }
 }
